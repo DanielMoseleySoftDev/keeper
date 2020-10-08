66f5ec3190fa04747c95f6c8c96c4193e03592db
Dan <drmsoftdev@gmail.com>
2020-10-08 19:55:02 +0100

    Updates

---
+++ precache-manifest.bead24ec79345ac5852d45af6d91b240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52da052985484a2a948c724779bf044b",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "8f811842ba026ba1b415",
+    "url": "/keeper/static/js/2.78a63b97.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/keeper/static/js/2.78a63b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed8e3f9621d1b3feb7b",
+    "url": "/keeper/static/js/main.40ed2114.chunk.js"
+  },
+  {
+    "revision": "b0f3c69d11e458d5f477",
+    "url": "/keeper/static/js/runtime-main.3e2bed01.js"
+  }
+]);
